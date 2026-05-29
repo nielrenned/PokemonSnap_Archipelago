83f4bc02458447057714721760dd19d463bc46ce
SomeJakeGuy <somejakeguy@gmail.com>
2026-05-29 10:18:45 -0400

    Added common PJ64 errors to the constants file for now.

--- apworld/pokemon_snap/constants.py
+++ apworld/pokemon_snap/constants.py
@@ -1,2 +1,6 @@
 GAME_NAME: str = "Pokemon Snap"
 CLIENT_NAME: str = "Pokemon Snap Client"
+
+INITIAL_STATUS: str = "Waiting to connect to Project 64"
+WRONG_GAME: str = "Wrong game was detected, please load a North American version of Pokemon Snap"
+CONNECTED_STATUS: str = "Successfully connected to Project64 and detected Pokemon Snap."
