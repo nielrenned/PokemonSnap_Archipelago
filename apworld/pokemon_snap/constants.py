90834e0529a0f3f5b70f6917dd679bfbbdeb0b54
SomeJakeGuy <somejakeguy@gmail.com>
2026-05-30 01:14:31 -0400

    Added disconnected status

--- apworld/pokemon_snap/constants.py
+++ apworld/pokemon_snap/constants.py
@@ -2,5 +2,6 @@ GAME_NAME: str = "Pokemon Snap"
 CLIENT_NAME: str = "Pokemon Snap Client"
 
 INITIAL_STATUS: str = "Waiting to connect to Project 64"
+DISCONNECTED_STATUS: str = "Unable to connect to Project 64. Attempting again in 5 seconds..."
 WRONG_GAME: str = "Wrong game was detected, please load a North American version of Pokemon Snap"
 CONNECTED_STATUS: str = "Successfully connected to Project64 and detected Pokemon Snap."
