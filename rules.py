1d8ad516c122eced61d5c1f8c3bc1b583f892fba
Jacob Dennerlein <nielrenned@gmail.com>
2026-08-10 21:19:06 -0400

    Add logic for oak rewards

--- rules.py
+++ rules.py
@@ -1,8 +1,10 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, override
+from dataclasses import dataclass
+from itertools import product
 
-from rule_builder.rules import Has, HasAll, HasAny, And, Rule
+from rule_builder.rules import Has, HasAll, HasAny, And, Rule, AtLeast, CanReachLocation, CollectionState, True_, False_
 from .items import SIGN_PIC_NAMES
-from .locations import wonderful, multiple, secret_exit, course, bonus, PokemonSnapLocationCategory as Category
+from .locations import wonderful, multiple, secret_exit, course, bonus, PokemonSnapLocationCategory as Category, species_data_tables
 from .constants import *
 
 if TYPE_CHECKING:
@@ -21,12 +23,15 @@ location_name_functions = {
     Category.SPECIAL_POSE: (lambda s: s),
     Category.POKEMON_SIGN: (lambda s: s),
     Category.SECRET_EXIT: secret_exit,
+    Category.OAK_REWARD: (lambda s: s),
 }
+
+def location_name(name: str, category: Category):
+    return location_name_functions[category](name)
 
 def set_location_rule(world: "PokemonSnapWorld", name: str, category: Category, rule: Rule):
     try:
-        name_func = location_name_functions[category]
-        base_name = name_func(name)
+        base_name = location_name(name, category)
         world.set_rule(world.get_location(base_name), rule)
         # Make sure the bonus locations also have the same rules
         world.set_rule(world.get_location(bonus(base_name)), rule)
@@ -48,6 +53,111 @@ def set_rules(world: "PokemonSnapWorld"):
         for category, rule in categories.items():
             set_location_rule(world, name, category, rule)
 
+    set_oak_rules(world)
+
+
+def set_oak_rules(world: "PokemonSnapWorld"):
+    can_reach_all_pokemon = [
+        CanReachLocation(pokemon_name)
+        for pokemon_name in ALL_INGAME_POKEMON
+        if pokemon_name not in POKEMON_IN_MULTIPLE_LEVELS
+    ] + [
+        CanReachLocation(course(pokemon_name, level))
+        for pokemon_name, level in product(POKEMON_IN_MULTIPLE_LEVELS, ALL_LEVELS)
+        if course(pokemon_name, level) in LOCATION_RULES
+    ]
+
+    set_location_rule(world, POKEMON_TOTAL_LVL_1, Category.OAK_REWARD, AtLeast(6, *can_reach_all_pokemon))
+    set_location_rule(world, POKEMON_TOTAL_LVL_2, Category.OAK_REWARD, AtLeast(22, *can_reach_all_pokemon))
+    set_location_rule(world, POKEMON_TOTAL_LVL_3, Category.OAK_REWARD, AtLeast(40, *can_reach_all_pokemon))
+
+    set_location_rule(world, REPORT_SCORE_LVL_1, Category.OAK_REWARD, ReportScoreAchievable( 24_000))
+    set_location_rule(world, REPORT_SCORE_LVL_2, Category.OAK_REWARD, ReportScoreAchievable( 72_500))
+    set_location_rule(world, REPORT_SCORE_LVL_3, Category.OAK_REWARD, ReportScoreAchievable(130_000))
+
+
+@dataclass()
+class ReportScoreAchievable(Rule["PokemonSnapWorld"], game="Pokemon Snap"):
+    """
+    This is hacky at best, but works for now. 
+
+    In _instantiate, for each pokemon, we create a list of location rules
+    that are independent of the world options (otherwise wonderful/multiple 
+    would be inaccessible). This list is passed to the Resolved rule. This
+    will probably need to be revisited when we add progressive lenses.
+
+    In the resolved rule, we heuristically estimate an upper bound for
+    the achievable score given the current game state, based on some
+    crude estimates for each line on the score sheet. Notably, we are
+    ignoring special poses. This is potentially a To-Do, but I think
+    the logic will actually be nicer without it.
+    """
+    score_goal: int
+
+    @override
+    def _instantiate(self, world: "PokemonSnapWorld") -> Rule.Resolved:
+        all_pokemon_rules = [
+            (self.build_rules_triplet(world, pokemon_name), ) # 1-tuple for pokemon only in one course
+            for pokemon_name in ALL_INGAME_POKEMON
+            if pokemon_name not in POKEMON_IN_MULTIPLE_LEVELS
+        ]
+
+        for pokemon_name in POKEMON_IN_MULTIPLE_LEVELS:
+            this_pokemon_rules = []
+            for level_name in ALL_LEVELS:
+                location_name = course(pokemon_name, level_name)
+                if location_name not in LOCATION_RULES: continue
+
+                this_pokemon_rules.append(self.build_rules_triplet(world, location_name))
+            all_pokemon_rules.append(tuple(this_pokemon_rules))
+
+        return self.Resolved(self.score_goal, tuple(all_pokemon_rules), player=world.player)
+
+    def build_rules_triplet(self, world: "PokemonSnapWorld", pokemon_name: str):
+        species_data = [species_data for region in species_data_tables for species_data in species_data_tables[region] if species_data.name == pokemon_name][0]
+        
+        normal_rule = CanReachLocation(location_name(pokemon_name, Category.NORMAL_PHOTO))
+
+        wonderful_rule = False_()
+        if species_data.wonderful:
+            if (pokemon_name not in LOCATION_RULES or Category.WONDERFUL_PHOTO not in LOCATION_RULES[pokemon_name]):
+                wonderful_rule = normal_rule
+            else:
+                wonderful_rule = And(normal_rule, LOCATION_RULES[pokemon_name][Category.WONDERFUL_PHOTO])
+
+        multiple_rule = False_()
+        if species_data.multiple:
+            if (pokemon_name not in LOCATION_RULES or Category.MULTIPLE_PHOTO not in LOCATION_RULES[pokemon_name]):
+                multiple_rule = wonderful_rule
+            else:
+                multiple_rule = And(wonderful_rule, LOCATION_RULES[pokemon_name][Category.MULTIPLE_PHOTO])
+
+        return normal_rule.resolve(world), wonderful_rule.resolve(world), multiple_rule.resolve(world)
+
+    class Resolved(Rule.Resolved):
+        score_goal: int
+        all_pokemon_rules: tuple[tuple[tuple[Rule.Resolved, Rule.Resolved, Rule.Resolved]]]
+
+        @override
+        def _evaluate(self, state: CollectionState) -> bool:
+            achievable_score_total = 0
+            for rule_set in self.all_pokemon_rules:
+                achievable_score_total += max((self.achievable_score(state, *rule_tuple) for rule_tuple in rule_set), default=0)
+                if achievable_score_total >= self.score_goal: return True
+            return False
+
+        def achievable_score(self, state: CollectionState, normal_rule: Rule.Resolved, wonderful_rule: Rule.Resolved, multiple_rule: Rule.Resolved):
+            # This is a heuristic, as it can change drastically between pokemon
+            if not normal_rule(state): return 0
+
+            score = 200
+            if wonderful_rule(state):
+                score += 900 # 150 more for size, and 750 more for pose
+                score *= 2
+            if multiple_rule(state):
+                score += 150
+
+            return score
 
 LOCATION_RULES = {
     # Beach
