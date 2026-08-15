8de5d7054b86ac133eba827a33159be7e4c4e1b3
Coby West <4885716+cobyw@users.noreply.github.com>
2026-08-15 17:27:25 -0500

    remove duplicated fill_slot_data define

--- __init__.py
+++ __init__.py
@@ -219,18 +219,6 @@ class PokemonSnapWorld(World):
             f"{self.multiworld.get_out_file_name_base(self.player)}{patch.patch_file_ending}")
         patch.write(out_path)
 
-    def fill_slot_data(self):
-        return {
-            "photo_bonuses": self.options.photo_bonuses.value,
-            "special_poses": self.options.special_poses.value,
-            "pokemon_signs": self.options.pokemon_signs.value,
-            "secret_exits": self.options.secret_exits.value,
-            "report_photo_count": self.options.report_photo_count.value,
-            "report_score_total": self.options.report_score_total.value,
-
-            "rng_checks": self.options.rng_checks.value,
-            "hard_checks": self.options.hard_checks.value,
-        }
     def fill_slot_data(self) -> dict[str, Any]:
         return self.options.as_dict(
             "goal_type", "signs_required", "pokemon_required",
